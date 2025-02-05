b35232bd1c4f9ce34eb774063997b5909ef6e7af
mark-ianz <bustillomarkian23@gmail.com>
2025-02-05 16:20:05 +0800

    company logo

---
+++ src/components/header/CompanyLogo.tsx
@@ -0,0 +1,7 @@
+import company_logo from "../../assets/COMPONY LOGO NO BG.png"
+
+export default function CompanyLogo() {
+  return (
+    <img src={company_logo} alt="barangay silangan solid waste management logo" />
+  )
+}
