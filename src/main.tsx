923ca47e4421172b06f749f2572b9a0d589d9d06
mark-ianz <bustillomarkian23@gmail.com>
2025-02-05 17:58:43 +0800

    initialized routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
+import EducationAndAwareness from "./pages/EducationAndAwareness.tsx";
+import Dummy from "./pages/Dummy.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "education-and-awareness",
+        element: <EducationAndAwareness />,
+      },
+    ],
+  },
+  {
+    path: "/dummy",
+    element: <Dummy/>
+  }
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
