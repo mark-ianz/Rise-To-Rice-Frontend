339551dd78b1bb224fd4a8e220cb2e5171608bd0
mark-ianz <bustillomarkian23@gmail.com>
2025-02-05 19:47:47 +0800

    added an optional class "wrapperClassName" and "inputClassName"

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,15 +1,18 @@
+import { cn } from "@/lib/utils";
 import { Input } from "./ui/input";
 
 type Props = {
   label: string;
   placeholder?: string;
+  inputClassName?: string;
+  wrapperClassName?: string
 };
 
-export default function InputText({label, placeholder}: Props) {
+export default function InputText({ label, placeholder, inputClassName, wrapperClassName }: Props) {
   return (
-    <span>
+    <span className={cn("flex flex-col", wrapperClassName)}>
       <p>{label}</p>
-      <Input placeholder={placeholder} className="bg-white"/>
+      <Input placeholder={placeholder} className={cn("bg-white", inputClassName)} />
     </span>
   );
 }
