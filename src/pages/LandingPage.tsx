4c03a6a1211ad463ad7d082d703c15100460e9a6
mark-ianz <bustillomarkian23@gmail.com>
2025-02-05 18:20:09 +0800

    seperated the wrapper into a component to make it reusable

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,23 @@
 import ButtonMain from "@/components/buttons/ButtonMain";
+import CenterLayout from "@/components/CenterLayout";
 import TextLogo from "@/components/logo/TextLogo";
 import SolidWasteManagement from "@/components/SolidWasteManagementProgram";
+import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+
   return (
-    <div className="h-full flex flex-col justify-center">
-      <TextLogo className="text-6xl"/>
-      <SolidWasteManagement className="text-lg -mt-4"/>
-      <ButtonMain variant={"default"} className="w-fit mt-2">Join Now</ButtonMain>
-    </div>
-  )
+    <CenterLayout>
+      <TextLogo className="text-6xl" />
+      <SolidWasteManagement className="text-lg -mt-4" />
+      <ButtonMain
+        onClick={() => navigate("/register")}
+        variant={"default"}
+        className="w-fit mt-2"
+      >
+        Join Now
+      </ButtonMain>
+    </CenterLayout>
+  );
 }
