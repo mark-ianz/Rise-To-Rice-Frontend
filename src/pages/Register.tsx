f78ff3370cf26875a94960155060918ad223e6fa
mark-ianz <bustillomarkian23@gmail.com>
2025-02-06 19:16:18 +0800

    Used the GoBackButton component

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,17 +1,15 @@
 import CenterLayout from "@/components/CenterLayout";
+import GoBackButton from "@/components/GoBackButton";
 import LeftSide from "@/components/register/LeftSide";
 import RightSide from "@/components/register/RightSide";
-import { ArrowLeft } from "lucide-react";
 
 export default function Register() {
   return (
     <CenterLayout className="items-center py-10">
       <div className="w-full h-full flex rounded-xl shadow-lg bg-red-50 relative">
-        <div className="absolute text-gray-500 top-4 left-4">
-          <ArrowLeft/>
-        </div>
+        <GoBackButton />
         <LeftSide />
-        <RightSide/>
+        <RightSide />
       </div>
     </CenterLayout>
   );
