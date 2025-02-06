57c5daa78d8a649cb589eb7c0221ea34f7de5e17
mark-ianz <bustillomarkian23@gmail.com>
2025-02-06 19:15:10 +0800

    added a function that redirects to login when clicking login

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import ButtonMain from "../buttons/ButtonMain";
 import CompanyLogo from "../logo/CompanyLogo";
 import TextLogo from "../logo/TextLogo";
@@ -5,17 +6,21 @@ import SolidWasteManagement from "../SolidWasteManagementProgram";
 import NavLinks from "./NavLinks";
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className="bg-secondary-light p-4 px-20">
       <div className="flex items-center gap-2">
-        <CompanyLogo/>
+        <CompanyLogo />
         <div>
           <TextLogo />
           <SolidWasteManagement />
         </div>
         <div className="flex-1 flex justify-end items-center gap-10">
           <NavLinks />
-          <ButtonMain variant={"default"}>Login</ButtonMain>
+          <ButtonMain variant={"default"} onClick={() => navigate("/login")}>
+            Login
+          </ButtonMain>
         </div>
       </div>
     </header>
