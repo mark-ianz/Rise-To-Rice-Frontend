3dc90eb23e97e4bc50c163696ea36fd85b13c5f0
mark-ianz <bustillomarkian23@gmail.com>
2025-02-06 19:15:35 +0800

    added the input type

--- src/components/register/Field_FirstSection.tsx
+++ src/components/register/Field_FirstSection.tsx
@@ -6,16 +6,25 @@ export default function Field_FirstSection() {
   return (
     <div className="flex flex-col mt-10 mb-10 gap-4">
       <div className="flex flex-col gap-4">
-        <InputText label="First Name" />
-        <InputText label="Middle Name" placeholder="(Optional)" />
+        <InputText type="text" label="First Name" />
+        <InputText type="text" label="Middle Name" placeholder="(Optional)" />
         <div className="flex gap-4">
-          <InputText label="Last Name" wrapperClassName="flex-1"/>
+          <InputText type="text" label="Last Name" wrapperClassName="flex-1" />
           <SelectSuffix />
         </div>
       </div>
       <div className="flex flex-row gap-4">
-        <InputText label="Address" wrapperClassName="flex-1" />
-        <InputText label="Contact Number" wrapperClassName="w-[180px]" />
+        <InputText
+          type="text"
+          label="Address"
+          wrapperClassName="flex-1"
+          placeholder="Blk, Street, Barangay, City"
+        />
+        <InputText
+          type="text"
+          label="Contact Number"
+          wrapperClassName="w-[180px]"
+        />
         <span>
           <p>Date of Birth</p>
           <DatePicker />
