20df2cd4bdbf9f61dfa7bd493142e8a43b736472
mark-ianz <bustillomarkian23@gmail.com>
2025-02-06 21:25:14 +0800

    CreateAccountState type

---
+++ src/types/createAccount.type.tsx
@@ -0,0 +1,15 @@
+export type CreateAccountState = {
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+  suffix: string;
+  address: string;
+  contact_number: string;
+  date_of_birth: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  error: string;
+  success: string;
+  loading: boolean;
+};
