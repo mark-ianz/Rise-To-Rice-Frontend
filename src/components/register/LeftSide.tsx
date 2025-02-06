fae7fe593047909e6d5fa835469991b3fe989235
mark-ianz <bustillomarkian23@gmail.com>
2025-02-06 19:15:52 +0800

    implemented the HeaderText component

--- src/components/register/LeftSide.tsx
+++ src/components/register/LeftSide.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
 import MeatBalls from "@/assets/meatballs-menu-svgrepo-com.svg";
 import Field_FirstSection from "./Field_FirstSection";
+import HeaderText from "../HeaderText";
 
 export default function LeftSide() {
   return (
     <div className="w-1/2 rounded-l-xl bg-secondary-light flex p-10 px-20 flex-col">
-      <p className="text-3xl font-coiny text-primary-main">REGISTER</p>
-      <Field_FirstSection/>
+      <HeaderText>Register</HeaderText>
+      <Field_FirstSection />
       <div className="flex flex-col items-center justify-center">
         <Button className="w-full text-lg">Continue</Button>
         <img className="w-6" src={MeatBalls} alt="options" />
