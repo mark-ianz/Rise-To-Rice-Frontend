0620fd38aae19448ca5eb2b2295c68ea1d8a2e09
mark-ianz <bustillomarkian23@gmail.com>
2025-02-06 21:25:28 +0800

    handle the changes on form inputs

--- src/components/register/Field_FirstSection.tsx
+++ src/components/register/Field_FirstSection.tsx
@@ -1,33 +1,79 @@
 import InputText from "@/components/InputText";
 import SelectSuffix from "@/components/SelectSuffix";
 import DatePicker from "../DatePicker";
+import useCreateAccountContext from "@/hooks/useCreateAccountContext";
 
 export default function Field_FirstSection() {
+  const { dispatch } = useCreateAccountContext();
+
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "SET_FIRST_NAME", payload: e.target.value });
+  };
+
+  const handleMiddleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "SET_MIDDLE_NAME", payload: e.target.value });
+  };
+
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "SET_LAST_NAME", payload: e.target.value });
+  };
+
+  const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "SET_ADDRESS", payload: e.target.value });
+  };
+
+  const handleContactNumberChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    dispatch({ type: "SET_CONTACT_NUMBER", payload: e.target.value });
+  };
+
+  const handleBirthDateChange = (date: Date | undefined) => {
+    if (!date) return;
+    dispatch({ type: "SET_DATE_OF_BIRTH", payload: date.toString() });
+  }
+
   return (
     <div className="flex flex-col mt-10 mb-10 gap-4">
       <div className="flex flex-col gap-4">
-        <InputText type="text" label="First Name" />
-        <InputText type="text" label="Middle Name" placeholder="(Optional)" />
+        <InputText
+          onChange={handleFirstNameChange}
+          type="text"
+          label="First Name"
+        />
+        <InputText
+          onChange={handleMiddleNameChange}
+          type="text"
+          label="Middle Name"
+          placeholder="(Optional)"
+        />
         <div className="flex gap-4">
-          <InputText type="text" label="Last Name" wrapperClassName="flex-1" />
+          <InputText
+            onChange={handleLastNameChange}
+            type="text"
+            label="Last Name"
+            wrapperClassName="flex-1"
+          />
           <SelectSuffix />
         </div>
       </div>
       <div className="flex flex-row gap-4">
         <InputText
+          onChange={handleAddressChange}
           type="text"
           label="Address"
           wrapperClassName="flex-1"
           placeholder="Blk, Street, Barangay, City"
         />
         <InputText
+          onChange={handleContactNumberChange}
           type="text"
           label="Contact Number"
-          wrapperClassName="w-[180px]"
+          wrapperClassName="flex-1"
         />
-        <span>
+        <span className="flex-1">
           <p>Date of Birth</p>
-          <DatePicker />
+          <DatePicker onChange={handleBirthDateChange} />
         </span>
       </div>
     </div>
