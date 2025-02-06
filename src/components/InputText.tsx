1962cd8a739b159f1bc666712f5668603461dc67
mark-ianz <bustillomarkian23@gmail.com>
2025-02-06 19:14:39 +0800

    added a type prop

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,18 +1,30 @@
 import { cn } from "@/lib/utils";
 import { Input } from "./ui/input";
+import { HTMLInputTypeAttribute } from "react";
 
 type Props = {
   label: string;
   placeholder?: string;
   inputClassName?: string;
-  wrapperClassName?: string
+  wrapperClassName?: string;
+  type: HTMLInputTypeAttribute;
 };
 
-export default function InputText({ label, placeholder, inputClassName, wrapperClassName }: Props) {
+export default function InputText({
+  label,
+  placeholder,
+  inputClassName,
+  wrapperClassName,
+  type,
+}: Props) {
   return (
     <span className={cn("flex flex-col", wrapperClassName)}>
       <p>{label}</p>
-      <Input placeholder={placeholder} className={cn("bg-white", inputClassName)} />
+      <Input
+        type={type}
+        placeholder={placeholder}
+        className={cn("bg-white", inputClassName)}
+      />
     </span>
   );
 }
